870a3a9c03d40f601ac733f552fc6304ab59e7a1
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-07-01 00:04:35 -0700

    setup sass & cssmin

--- gruntfile.js
+++ gruntfile.js
@@ -7,15 +7,55 @@ module.exports = grunt => {
         files: [
           {
             expand: true,
-            cwd: './public',
+            cwd: 'src/public',
             src: ['**'],
-            dest: './dist/public'
+            dest: 'dist'
+          }
+        ]
+      }
+    },
+
+    pug: {
+      compile: {
+        options: {
+          data: {
+            debug: true
           },
+          pretty: true
+        },
+        files: [
+          {
+            src: '**/*.pug',
+            cwd: 'src/views',
+            dest: 'dist',
+            expand: true,
+            ext: '.html'
+          }
+        ]
+      }
+    },
+    sass: {
+      dist: {
+        files: [
+          {
+            src: '**/*.scss',
+            cwd: 'src/sass',
+            dest: 'dist/css',
+            expand: true,
+            ext: '.css'
+          }
+        ]
+      }
+    },
+    cssmin: {
+      target: {
+        files: [
           {
             expand: true,
-            cwd: './src/views',
-            src: ['**'],
-            dest: './dist/views'
+            cwd: 'dist/css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'dist/css',
+            ext: '.css'
           }
         ]
       }
@@ -24,7 +64,7 @@ module.exports = grunt => {
       app: {
         files: [
           {
-            src: ['src/**/*.ts', '!src/.baseDir.ts'],
+            src: ['src/**/*.ts', '!node_modules/**/*.ts'],
             dest: './dist'
           }
         ],
@@ -41,16 +81,33 @@ module.exports = grunt => {
         files: ['src/**/*.ts'],
         tasks: ['ts']
       },
-      views: {
-        files: ['views/**/*.pug'],
+      pug: {
+        files: ['src/views/**/*.pug'],
+        tasks: ['pug']
+      },
+      sass: {
+        files: ['src/sass/**/*.sass'],
+        tasks: ['sass']
+      },
+      copy: {
+        files: ['src/libraries/**', 'src/public/**'],
         tasks: ['copy']
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['copy', 'ts']);
+  if (process.env.NODE_ENV === 'production') {
+    grunt.registerTask('default', ['ts', 'pug', 'sass', 'copy', 'cssmin']);
+  } else {
+    grunt.registerTask('default', ['ts', 'pug', 'sass', 'copy', 'watch']);
+  }
 };
